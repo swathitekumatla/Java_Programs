8bab39fab2fad6f592add7c0351f190f5faa55f5
swathitekumatla <tekumatlaswathi7@gmail.com>
2023-10-28 11:46:04 +0530

    This is my First Commit for practice

---
+++ src/com/swathi/swathi/swathi.java
@@ -0,0 +1,10 @@
+package com.swathi.swathi;
+
+public class swathi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                 System.out.print("swathi");
+	}
+
+}
